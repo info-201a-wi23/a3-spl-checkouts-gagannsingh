91bd74c839e83ec7fe237e2c0133a060ed4aebe0
Your Name <you@example.com>
2023-02-26 23:08:07 -0800

    Charts done

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,28 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+#Load Library
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+
+# Read Data
+df <- read.csv("C:/Users/gagan/OneDrive/Desktop/info201/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+# Filter data for book, ebook, and audiobook
+df_subset <- subset(df, MaterialType %in% c("BOOK", "EBOOK", "AUDIOBOOK"))
+
+# Group the data by MaterialType and CheckoutMonth and calculate the sum of Checkouts
+checkout_by_material_month <- aggregate(Checkouts ~ MaterialType + CheckoutMonth, df_subset, sum)
+
+# Create a line chart showing the monthly checkout trends for different MaterialTypes
+ggplot(checkout_by_material_month, aes(x=CheckoutMonth, y=Checkouts, color=MaterialType)) + 
+  geom_line(size=1) + 
+  scale_color_brewer(palette="Dark2") +
+  labs(title="Monthly Checkout Trends of Books, eBooks, and Audiobooks in 2022",
+       x="Month", y="Total Checkouts",
+       color="Material Type") +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position="bottom",
+        legend.text = element_text(size = 10)) +
+  scale_x_continuous(breaks = seq(1, 12, 1)) +
+  scale_y_continuous(labels = scales::number_format(scale = 1e-3, suffix = "k")) +
+  geom_text(data = subset(checkout_by_material_month, CheckoutMonth == 12),
+            aes(x = CheckoutMonth, y = Checkouts, label = round(Checkouts/1000, 1)),
+            size = 3, vjust = -1)
+
--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,24 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+#Load Library
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+
+# Read Data
+df <- read.csv("C:/Users/gagan/OneDrive/Desktop/info201/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+# Group the data by UsageClass and CheckoutMonth and calculate the sum of Checkouts
+checkout_by_usage_month <- aggregate(Checkouts ~ UsageClass + CheckoutMonth, df, sum)
+
+# Create a line chart showing the monthly checkout trends for different UsageClasses
+ggplot(checkout_by_usage_month, aes(x=CheckoutMonth, y=Checkouts, color=UsageClass)) + 
+  geom_line(size=1) + 
+  scale_color_brewer(palette="Dark2") +
+  labs(title="Monthly Checkout Trends by Usage Class in 2022",
+       x="Month", y="Total Checkouts",
+       color="Usage Class") +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position="bottom") +
+  scale_x_continuous(breaks = seq(1, 12, 1)) +
+  scale_y_continuous(labels = scales::number_format(scale = 1e-3, suffix = "k"))
+
+
+
+
